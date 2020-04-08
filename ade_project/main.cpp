8329cc18052349fdec120b9de56dbbbcd3b326e3
Anthony Peres da Cruz <anthony.peresdacruz@gmail.com>
2020-04-08 14:07:21 +0200

    Initial Commit

---
+++ ade_project/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ade_project
+//
+//  Created by Anthony Peres da Cruz on 08/04/2020.
+//  Copyright © 2020 Anthony Peres da Cruz. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
